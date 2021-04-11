57a60f2a67162b9ec1f29c34a38cd1fea52a08d6
Martin <martinty@users.noreply.github.com>
2021-04-11 15:25:27 +0200

    Add liveCode files

---
+++ liveCode.cpp
@@ -0,0 +1,4 @@
+
+void runLiveCode() {
+    // Write live code
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#include "liveCode.h"
 #include "multiThreading.h"
 // #include "testEigen.h"
 
 int main() {
     // showEigenUsage();
+
     testThreadsMenu();
+
+    runLiveCode();
 
     return 0;
 }
